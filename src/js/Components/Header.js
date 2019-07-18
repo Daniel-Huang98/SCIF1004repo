d5c1061910d1ace447261bea685826cf9f1ffb38
Daniel Huang <danielhuang@pop-os.localdomain>
2019-07-18 15:29:24 +1000

    made slight changes to the margins of text

--- src/js/Components/Header.js
+++ src/js/Components/Header.js
@@ -4,7 +4,8 @@ class Top extends React.Component{
     render(){
         return(
             <header id="Header">
-                <h1>Science in the Cinema</h1>
+                <h1>Science in the Cinema:</h1>
+                <h2>Contagion and Outbreak</h2>
             </header>
         );
     }
--- src/js/data.js
+++ src/js/data.js
@@ -31,6 +31,83 @@ var data = [
         caption: "Caption 6",
         meme: require("./Images/Meme6.jpg"),
         discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 7",
+        meme: require("./Images/Meme1.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 8",
+        meme: require("./Images/Meme2.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 9",
+        meme: require("./Images/Meme3.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 10",
+        meme: require("./Images/Meme4.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 11",
+        meme: require("./Images/Meme5.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 12",
+        meme: require("./Images/Meme6.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 13",
+        meme: require("./Images/Meme1.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 14",
+        meme: require("./Images/Meme2.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 15",
+        meme: require("./Images/Meme3.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 16",
+        meme: require("./Images/Meme4.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 17",
+        meme: require("./Images/Meme5.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 18",
+        meme: require("./Images/Meme6.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+    },
+    {
+        caption: "Caption 19",
+        meme: require("./Images/Meme5.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
+
+    },
+    {
+        caption: "Caption 20",
+        meme: require("./Images/Meme6.jpg"),
+        discussion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in odio at neque bibendum hendrerit. Nunc sodales mi vitae consectetur aliquet. Suspendisse ornare facilisis purus, a blandit felis mattis vitae. Etiam sit amet blandit mi. Sed cursus, lacus vitae rutrum congue, lacus lorem consequat odio, ut aliquet diam orci ac felis. Morbi nec porta nunc, blandit tristique felis. Mauris scelerisque, diam nec rhoncus malesuada, odio lorem placerat metus, vel suscipit est magna eget lacus. In id sagittis nunc. Vivamus sed odio eget risus molestie ultrices nec vel neque. Aliquam erat volutpat. Phasellus imperdiet nisl id purus eleifend maximus. Duis commodo."
     }
 ]
 
